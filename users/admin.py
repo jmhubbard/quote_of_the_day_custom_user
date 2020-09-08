ea8e6adb828dcfaae03f59d3269cb4f7b0c578a0
Jason Hubbard <jasonhubb@gmail.com>
2020-09-07 20:46:00 -0700

    created shows app

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+
+# class UserAdmin(BaseUserAdmin):
+#     # The forms to add and change user instances
+#     form = UserAdminChangeForm
+#     add_form = UserAdminCreationForm
+
+#     # The fields to be used in displaying the User model.
+#     # These override the definitions on the base UserAdmin
+#     # that reference specific fields on auth.User.
+#     list_display = ('email', 'is_active', 'is_staff','is_superuser', 'date_joined')
+#     list_filter = ('is_active','is_staff','is_superuser','date_joined')
+#     fieldsets = (
+#         (None, {'fields': ('email', 'password',)}),
+#         ('Personal info', {'fields': ()}),
+#         ('Permissions', {'fields': ('is_active','is_staff','is_superuser',)}),
+#     )
+#     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+#     # overrides get_fieldsets to use this attribute when creating a user.
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'password1', 'password2'),
+#         }),
+#     )
+#     search_fields = ('email',)
+#     ordering = ('email',)
+#     filter_horizontal = ()
 
 admin.site.register(User, UserAdmin)
 
--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = ['email']
 
 # Create your models here.
