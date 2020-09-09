8a8b0ea307ee3ac6eb0bd0c8047da2246472552a
Jason Hubbard <jasonhubb@gmail.com>
2020-09-08 23:50:25 -0700

    user can only have one subscription per show

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -7,3 +7,8 @@ class Subscription(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     show = models.ForeignKey('shows.Show', on_delete=models.CASCADE)
     is_subscribed = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = (
+            ("user", "show"),
+        )
