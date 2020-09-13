ae0ca8f92517b07af1101066e0d4ff830cf3d420
Jason Hubbard <jasonhubb@gmail.com>
2020-09-12 17:14:08 -0700

    modified user creation to automatically sign them up for all shows

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -2,7 +2,15 @@ from django.db import models
 import users.models
 import shows.models
 
-# Create your models here.
+def subscribe_user_on_creation(user, shows):
+    for show in shows:
+        subscription = Subscription()
+        subscription.user = user
+        subscription.show = show
+        subscription.save()
+    
+
+
 class Subscription(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     show = models.ForeignKey('shows.Show', on_delete=models.CASCADE)
@@ -16,3 +24,6 @@ class Subscription(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse("subscription-update", args=[self.id])
+
+    def __str__(self):
+        return (f'{self.user}    {self.show}    {self.is_subscribed}')
