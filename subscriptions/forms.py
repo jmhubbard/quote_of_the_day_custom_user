c642d124570ef1a1ed9d4e69ba3c201708ec5144
Jason Hubbard <jasonhubb@gmail.com>
2020-09-13 21:02:18 -0700

    removed old subscriptionform code

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -3,9 +3,6 @@ from subscriptions.models import Subscription
 from django.utils.translation import gettext as _
 
 class SubscriptionForm(forms.ModelForm):
-    # class LimitedStatus:
-    #     Subscription.Status.SUBSCRIBED
-    #     Subscription.Status.UNSUBSCRIBED
     STATUS_CHOICES = (
         (Subscription.STATUS_CHOICE_SUBSCRIBED, _('Subscribed')),
         (Subscription.STATUS_CHOICE_UNSUBSCRIBED, _('Unsubscribed')),
@@ -22,7 +19,6 @@ class SubscriptionForm(forms.ModelForm):
         usersubscription = super().save(commit=False)
         usersubscription.status = self.cleaned_data['status']
 
-        # userpreference.user = user
         if commit:
             usersubscription.save()
         return usersubscription
