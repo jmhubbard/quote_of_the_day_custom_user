39772ccaf0834ddebaf3d348b59c90b09d338fb3
Jason Hubbard <jasonhubb@gmail.com>
2020-09-14 18:12:26 -0700

    assigned unauthgenticated_user decorator to usercreate

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import User
 from .forms import UserForm
 from django.contrib.messages.views import SuccessMessageMixin
+from main.decorators import unauthenticated_user
+from django.utils.decorators import method_decorator
 
 
 class UserCreate(SuccessMessageMixin, CreateView):
@@ -11,3 +13,7 @@ class UserCreate(SuccessMessageMixin, CreateView):
     form_class = UserForm
     success_url = "/accounts/login/"
     success_message = "Account was successfully created."
+
+    @method_decorator(unauthenticated_user) #If user is already authenticated they will be redirected to their subscription page
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
