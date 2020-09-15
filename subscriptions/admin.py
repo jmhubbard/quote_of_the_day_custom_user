6cfc522e80f454bf6cb0410f34548f961d69c2e4
Jason Hubbard <jasonhubb@gmail.com>
2020-09-14 21:29:01 -0700

    added spaces to see code easier

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Subscription
 
+
+
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'show', 'is_subscribed')
+
 admin.site.register(Subscription, SubscriptionAdmin)
