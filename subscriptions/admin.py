013521f447753f5695df6acebac1ee545e5c2d12
Jason Hubbard <jasonhubb@gmail.com>
2020-09-15 01:06:22 -0700

    Created list filter in subscription admin

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -5,5 +5,6 @@ from .models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'show', 'status')
+    list_filter = ('show','status',)
 
 admin.site.register(Subscription, SubscriptionAdmin)
