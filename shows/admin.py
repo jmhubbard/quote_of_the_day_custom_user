e041a7a69f4d2167881f447471129959ec86c955
Jason Hubbard <jasonhubb@gmail.com>
2020-09-14 21:28:40 -0700

    created an admin action to create subscription for all shows if user doesn't already have them

--- shows/admin.py
+++ shows/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Show
+from users.models import User
+from subscriptions.models import Subscription
 
+
+
+def create_subscription_for_all_users_if_doesnt_exist(modeladmin, request, queryset):
+    all_users = User.objects.all()
+
+    for user in all_users:
+        user_shows = user.show_set.all()
+        for show in queryset:
+            if show not in user_shows:
+                # print(f'show is {show} and pref are {user_subscriptions)')
+                sub = Subscription(user=user, show=show)
+                sub.save()
 
 
 class ShowAdmin(admin.ModelAdmin):
+    actions = (
+    create_subscription_for_all_users_if_doesnt_exist,
+    )
     list_display = ('name',)
+
 admin.site.register(Show, ShowAdmin)
