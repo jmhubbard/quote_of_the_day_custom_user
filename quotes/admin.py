28bf8513cb889aa2e9436b8656df168de69f066d
Jason Hubbard <jasonhubb@gmail.com>
2020-09-21 00:23:50 -0700

    Created a quote model and quote admin page

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from quotes.models import Quote
 # Register your models here.
+
+class QuoteAdmin(admin.ModelAdmin):
+
+  
+    list_display = (  
+    'quote_text_abridged',
+    'quotelength',
+    # 'abridgedquotelength',
+    'speaker_name',
+    'show_name',
+    'season_number',
+    'episode_number',
+    'episode_title'
+    )
+
+    list_filter = (  
+    'speaker_name',
+    'show_name',
+    'season_number',
+    'episode_number',
+    'episode_title',
+    )
+
+
+    def quotelength(self, quote):
+        return len(quote.quote_text)
+
+    # def abridgedquotelength(self, quote):
+    #     return len(self.quote_text_abridged(quote))
+
+    def quote_text_abridged(self, quote):
+        maxlength = 30
+        if len(quote.quote_text) <= maxlength:
+            return quote.quote_text
+        else:
+            # we return a string of length maxlength by slicing the string of maxlength - 1 
+            # to account for the addition of the '…'
+            return (quote.quote_text[:maxlength-1] + '…' )
+    quote_text_abridged.short_description = 'Abridged Quote'
+
+
+admin.site.register(Quote, QuoteAdmin)
--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from shows.models import Show
 
 # Create your models here.
+class Quote(models.Model):
+    quote_text = models.TextField(unique=True)
+    speaker_name = models.CharField(max_length=255)
+    show_name = models.ForeignKey(Show, on_delete=models.CASCADE)
+    season_number = models.CharField(max_length=255)
+    episode_number = models.CharField(max_length=255)
+    episode_title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'''"{self.quote_text}" by {self.speaker_name} - {self.show_name} - {self.season_number} - {self.episode_title}'''
+        # return f"{self.quote_text} by {self.speaker_name} - {self.show_name} - {self.season_number} - {self.episode_title}"
+
+
+    def get_email_representation(self):
+        return str(self)
