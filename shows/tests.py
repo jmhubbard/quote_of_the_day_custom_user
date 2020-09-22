9068ff396cd87f54c7b31c8878cd17deb2c5909f
Jason Hubbard <jasonhubb@gmail.com>
2020-09-21 20:31:34 -0700

    created tests for creating an episode, and testing that episode cannot be negative

--- shows/tests.py
+++ shows/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.db import IntegrityError
-from .models import Show
+from .models import Show, Episode
 # Create your tests here.
 class ShowNameTestCase(TestCase):
     def test_show_name_is_unique(self):
@@ -17,3 +17,15 @@ class ShowNameUniquenessTestCase(TestCase):
     def test_show_name_is_unique(self):
         with self.assertRaises(IntegrityError):
             Show.objects.create(name="a")
+
+class EpisodeCreateTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Show.objects.create(name="a")
+    def test_create_episode(self):
+        show = Show.objects.get(name="a")
+        Episode.objects.create(name="Episode Name", number=4, show=show)
+    def test_episode_number_cannot_be_negative(self):
+        show = Show.objects.get(name="a")
+        with self.assertRaises(IntegrityError):
+            Episode.objects.create(name="Episode Name", number=-4, show=show)
