398c8194056a8127078b70fb24432e7be1b30417
Jason Hubbard <jasonhubb@gmail.com>
2020-09-21 19:21:27 -0700

    Created test case for show name uniqueness

--- shows/tests.py
+++ shows/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.db import IntegrityError
+from .models import Show
 # Create your tests here.
+class ShowNameTestCase(TestCase):
+    def test_show_name_is_unique(self):
+        with self.assertRaises(IntegrityError):
+            Show.objects.create(name="a")
+            Show.objects.create(name="a")
+
+
+class ShowNameUniquenessTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Show.objects.create(name="a")
+
+    def test_show_name_is_unique(self):
+        with self.assertRaises(IntegrityError):
+            Show.objects.create(name="a")
