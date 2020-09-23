a0203253021743b91f2fd46e7b81fffff19d4394
Jason Hubbard <jasonhubb@gmail.com>
2020-09-22 21:40:03 -0700

    Added an episode field that links to episode model

--- quotes/models.py
+++ quotes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from shows.models import Show
+from shows.models import Show, Episode
 
 # Create your models here.
 class Quote(models.Model):
@@ -9,7 +9,8 @@ class Quote(models.Model):
     season_number = models.CharField(max_length=255)
     episode_number = models.CharField(max_length=255)
     episode_title = models.CharField(max_length=255)
-
+    episode = models.ForeignKey(Episode, on_delete=models.CASCADE, null=True)
+    
     def __str__(self):
         return f'''"{self.quote_text}" by {self.speaker_name} - {self.show_name} - {self.season_number} - {self.episode_title}'''
         # return f"{self.quote_text} by {self.speaker_name} - {self.show_name} - {self.season_number} - {self.episode_title}"
