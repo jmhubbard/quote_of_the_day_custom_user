10c663d7d4f826bec676369375739b5f2c73eb74
Jason Hubbard <jasonhubb@gmail.com>
2020-09-23 23:38:15 -0700

    Created a try statement incase show object doesn't exist

--- shows/management/commands/episode_loader.py
+++ shows/management/commands/episode_loader.py
@@ -3,6 +3,7 @@ from quotes.models import Episode, Show
 from quotes.quotelist import quotelistdict
 
 from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 
 class Command(BaseCommand):
     help = 'Loads episodes from quotes.quotelist.quotedictlist'
@@ -14,24 +15,31 @@ class Command(BaseCommand):
         savedCount = 0
         duplicateCount = 0
         totalattempteditems = 0
+        show_does_not_exist = []
         for item in quotelistdict:
             totalattempteditems += 1
-            episode = Episode(
-                name=item["episode"],
-                number=69,
-                season=item["season"],
-                show=Show.objects.get(name=item["show"])
-            )
-            if options["save"]:
-                try:
-                    episode.save()
-                except IntegrityError:
-                    duplicateCount += 1
-                    continue
-                else:
-                    savedCount += 1
+            try:
+                show_name=Show.objects.get(name=item["show"])
+            except ObjectDoesNotExist:
+                show_does_not_exist.append(item)
+                print('hello')
+            else:
+                episode = Episode(
+                    name=item["episode"],
+                    number=69,
+                    season=item["season"],
+                    show=show_name
+                )
+                if options["save"]:
+                    try:
+                        episode.save()
+                    except IntegrityError:
+                        duplicateCount += 1
+                        continue
+                    else:
+                        savedCount += 1
                     
-
+        print(f'Show does not exist in database for the follow: {show_does_not_exist}')
         print(f'Total attempted quotes: {totalattempteditems}')
         print(f"Skipped {duplicateCount} duplicate episodes.")
         print(f"Saved: {savedCount} episodes.")
