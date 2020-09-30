3ffed4337a293b15799c2c81785d9bdbd5fef3bd
Jason Hubbard <jasonhubb@gmail.com>
2020-09-29 19:09:59 -0700

    Created an emailtracker model

--- emails/models.py
+++ emails/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import users.models
 
-# Create your models here.
+class EmailTracker(models.Model):
+    user = models.OneToOneField("users.User", on_delete=models.CASCADE)
+    last_quote_email = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return f"{self.user}'s last daily email was sent on {self.last_quote_email}"
