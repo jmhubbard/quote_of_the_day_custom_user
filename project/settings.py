a5d2ccddcc545c16aa15242f465ba1e320ebf35c
Jason Hubbard <jasonhubb@gmail.com>
2020-10-01 17:01:28 -0700

    Installed whitenoise in settings middleware

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ SITE_ID = 1
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
