00ef9b12218e474f4ecb6587433b3e89a35d66de
Jason Hubbard <jasonhubb@gmail.com>
2020-10-03 01:04:00 -0700

    add delete_account url using UserDeleteView

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from users.views import UserCreate
+from users.views import UserCreate, UserDeleteView
 from main.views import UserLoginView, UserLogoutView
 
 # from django.contrib.auth import views as auth_views
@@ -7,7 +7,8 @@ from main.views import UserLoginView, UserLogoutView
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('add/', UserCreate.as_view(), name='user-add'), #Create user page
-    path('logout/' , UserLogoutView.as_view(), name='logout')
+    path('logout/' , UserLogoutView.as_view(), name='logout'),
+    path('delete_account/<int:pk>/', UserDeleteView.as_view(), name='delete_account')
         # path('', include('django.contrib.auth.urls')), #accounts/login = login page
         # path('accounts/', include('django.contrib.auth.urls')), #accounts/login = login page
 ]
