fac9378eebdb6a743a69468c0d4291ca11315b6c
Jason Hubbard <jasonhubb@gmail.com>
2020-10-04 10:02:04 -0700

    add acount_page url using useraccountview

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from users.views import UserCreate, UserDeleteView
+from users.views import UserCreate, UserDeleteView, UserAccountView, useraccountview
 from main.views import UserLoginView, UserLogoutView
 
 # from django.contrib.auth import views as auth_views
@@ -8,7 +8,9 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('add/', UserCreate.as_view(), name='user-add'), #Create user page
     path('logout/' , UserLogoutView.as_view(), name='logout'),
-    path('delete_account/<int:pk>/', UserDeleteView.as_view(), name='delete_account')
+    path('delete_account/<int:pk>/', UserDeleteView.as_view(), name='delete_account'),
+    path('account_page/', useraccountview, name='account_page')
         # path('', include('django.contrib.auth.urls')), #accounts/login = login page
         # path('accounts/', include('django.contrib.auth.urls')), #accounts/login = login page
+        
 ]
