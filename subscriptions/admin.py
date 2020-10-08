f82db16513fc988eb019fbd74936885729bcf72b
Jason Hubbard <jasonhubb@gmail.com>
2020-10-07 21:57:33 -0700

    add subscribe and unsubscribe to selected shows admin command

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from .models import Subscription
 
+def subscribe_to_selected_shows(modeladmin, request, queryset):
+    for subscription in queryset:
+        subscription.status = 1
+        subscription.save()
 
+def unsubscribe_to_selected_shows(modeladmin, request, queryset):
+    for subscription in queryset:
+        subscription.status = 2
+        subscription.save()
 
 class SubscriptionAdmin(admin.ModelAdmin):
+    actions = (
+        subscribe_to_selected_shows,
+        unsubscribe_to_selected_shows,
+    )
+
     list_display = ('user', 'show', 'status')
     list_filter = ('show','status',)
 
