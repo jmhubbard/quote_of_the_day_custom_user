5407c04371bc74576c1182afb035ba0c0f10f3a4
Jason Hubbard <jasonhubb@gmail.com>
2020-10-08 23:46:08 -0700

    create contactview

--- emails/views.py
+++ emails/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from emails.forms import ContactForm
+from django.shortcuts import redirect
 
-# Create your views here.
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+
+from django.contrib.messages.views import SuccessMessageMixin
+
+class ContactView(SuccessMessageMixin, FormView):
+    template_name = 'emails/contactform.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contactform')
+    success_message = 'Your message was sent'
+
+    def form_valid(self, form):
+        form.send_message()
+        return super().form_valid(form)
